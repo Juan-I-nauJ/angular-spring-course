01487b548a58d77a6bb851fb760413f882f7b54f
JuanI <juanpalaciagar@gmail.com>
2023-08-14 21:36:34 +0200

    Added logic to make use of the backend's api for creation, updating, deletion and listing clients both as a whole and individually. This included creating a form view to create and edit new/existing clients and adding validation logic to it. Also touched on several styles and added the sweetalerts2 library.

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import {Observable, of} from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {Observable, of, throwError} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {map, catchError} from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -11,14 +13,70 @@ import {map} from 'rxjs/operators';
 export class ClienteService {
 
 private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private router:Router) { }
 
   getClientes(): Observable<Cliente[]>{
     // return of(CLIENTES);
-    return this.http.get<Cliente[]>(this.urlEndPoint)
+    return this.http.get<Cliente[]>(this.urlEndPoint).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.error, 'error' );
+        return throwError(()=> new Error(e));
+  }
+      )
+    );
     // .pipe(
     //   map(response => response as Cliente[])
     //);
   }
+
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.error, 'error' );
+        return throwError(()=> new Error(e));
+  }
+      )
+    );
+}
+
+getCliente(id: number): Observable<Cliente>{
+  return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+    catchError(e => {
+      this.router.navigate(['/clientes']);
+      console.error(e.error.mensaje);
+      Swal.fire('Error al editar', e.error.mensaje, 'error' );
+      return throwError(()=> new Error(e));
+    })
+  );
+}
+
+update(cliente: Cliente): Observable<Cliente>{
+  return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+    catchError(e => {
+      console.error(e.error.mensaje);
+      Swal.fire(e.error.mensaje, e.error.error, 'error' );
+      return throwError(()=> new Error(e));
+}
+    )
+  );
+}
+
+delete(id: number):Observable<Cliente>{
+  return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders}).pipe(
+    catchError(e => {
+      console.error(e.error.mensaje);
+      Swal.fire(e.error.mensaje, e.error.error, 'error' );
+      return throwError(()=> new Error(e));
+}
+    )
+  );
+}
+
+
+
+
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -20,4 +21,31 @@ export class ClientesComponent {
       clientes => this.clientes = clientes
     );
   }
+
+delete(cliente: Cliente):void{
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: "No se puede recuperar una vez borrado.",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: '¡Dale loco!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.clienteService.delete(cliente.id).subscribe(
+        response => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente)
+          Swal.fire(
+            'Borrado exitoso',
+            `Cliente ${cliente.nombre} fué borrado con éxito.`,
+            'success'
+          );
+        }
+      );
+  
+    }
+  });
+}
+
 }
